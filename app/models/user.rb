2e7ab6fcba2e199ab02e76e6f3b297d1ab937f29
André Franco <andre.gfranco@gmail.com>
2015-10-12 16:12:21 +0100

    Adds custom fields 'first_name' and 'second_name' to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true, on: :create
+  validates :last_name, presence: true, on: :create
+
 end
