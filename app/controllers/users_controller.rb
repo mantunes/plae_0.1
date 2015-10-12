1ad7e1033820ef410b6df18b020a5acc257b1d66
André Franco <andre.gfranco@gmail.com>
2015-10-12 17:24:06 +0100

    Adds email fields to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
-	 before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
-    @users = User.all
-  end
+   if current_user
+    @user = current_user
+   else
+     redirect_to new_user_session_path, notice: 'You are not logged in.'
+   end
+   @users = User.all
+ end
 
-  def show
-    @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
+ def show
+  @user = User.find(params[:id])
+  unless @user == current_user
+    redirect_to :back, :alert => "Access denied."
   end
+end
 
 end
