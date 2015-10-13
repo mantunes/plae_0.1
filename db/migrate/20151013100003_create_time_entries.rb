10cb53d774ca92ef4c39ed7f806a9e75e7632bd2
André Franco <andre.gfranco@gmail.com>
2015-10-13 11:44:17 +0100

    Created 'TimeEntry' model and migration

---
+++ db/migrate/20151013100003_create_time_entries.rb
@@ -0,0 +1,11 @@
+class CreateTimeEntries < ActiveRecord::Migration
+  def change
+    create_table :time_entries do |t|
+      t.string :name
+      t.datetime :start_time
+      t.datetime :end_time
+      t.integer :total_time, limit: 8
+      t.timestamps null: false
+    end
+  end
+end
