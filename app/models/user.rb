e8044f664b6399f67e920778e25a177b2ed27101
André Franco <andre.gfranco@gmail.com>
2015-10-14 17:44:31 +0100

    Changed Identation in app controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,18 @@ class User < ActiveRecord::Base
 
 	validates :password,
 	format: {
-  	with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
-  	message: %(must have 1 upper case letter, 1 down case letter,
-  		and 1 number or special character)
-	}
+		with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
+		message: %(must have 1 upper case letter, 1 down case letter,
+			and 1 number or special character)
+	}, on: :create
+
+	validates :password,
+	format: {
+		with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
+		message: %(must have 1 upper case letter, 1 down case letter,
+			and 1 number or special character)
+	}, on: :update, allow_blank: true
+
 	validates :first_name, presence: true, on: :create
 	validates :last_name, presence: true, on: :create
 	
