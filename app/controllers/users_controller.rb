ec0f0419ec368a6f72c32108704a056612be16aa
André Franco <andre.gfranco@gmail.com>
2015-10-14 12:35:11 +0100

    Removed visitors controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-   if current_user
-    @user = current_user
-   else
-     redirect_to new_user_session_path, notice: 'You are not logged in.'
-   end
-   @users = User.all
- end
-
- def show
-  @user = User.find(params[:id])
-  unless @user == current_user
-    redirect_to :back, :alert => "Access denied."
+    if current_user
+      @user = current_user
+    else
+      redirect_to new_user_session_path, notice: 'You are not logged in.'
+    end
+      @users = User.all
   end
-end
 
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
 end
