880cb5eefe5433f324ddfca0990a0d2671620f29
André Franco <andre.gfranco@gmail.com>
2015-10-14 16:55:12 +0100

    Changes identation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
-	
+	devise :database_authenticatable, :registerable, 
+	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+
 	validates :password,
-  format: {
+	format: {
   	with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
   	message: %(must have 1 upper case letter, 1 down case letter,
   		and 1 number or special character)
 	}
 	validates :first_name, presence: true, on: :create
 	validates :last_name, presence: true, on: :create
-
+	
 end
