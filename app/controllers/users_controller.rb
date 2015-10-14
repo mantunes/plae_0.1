59bae76cc9984eef892c35cfd45188345a5d42df
André Franco <andre.gfranco@gmail.com>
2015-10-14 17:50:09 +0100

    Fixed Identation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def index
-		if current_user
-			@user = current_user
-		else
-			redirect_to new_user_session_path, notice: 'You are not logged in.'
-		end
-		@users = User.all
-	end
+  def index
+    if current_user
+      @user = current_user
+    else
+      redirect_to new_user_session_path, notice: 'You are not logged in.'
+    end
+    @users = User.all
+  end
 
-	def show
-		@user = User.find(params[:id])
-		unless @user == current_user
-			redirect_to :back, :alert => "Access denied."
-		end
-	end
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,24 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable, 
-	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+  devise :database_authenticatable, :registerable, 
+  :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
-	validates :password,
-	format: {
-		with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
-		message: %(must have 1 upper case letter, 1 down case letter,
-			and 1 number or special character)
-	}, on: :create
+  validates :password,
+  format: {
+    with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
+    message: %(must have 1 upper case letter, 1 down case letter,
+      and 1 number or special character)
+  }, on: :create
 
-	validates :password,
-	format: {
-		with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
-		message: %(must have 1 upper case letter, 1 down case letter,
-			and 1 number or special character)
-	}, on: :update, allow_blank: true
+  validates :password,
+  format: {
+    with: /(?=.*[\d\W])(?=.*[A-Z])(?=.*[a-z])/,
+    message: %(must have 1 upper case letter, 1 down case letter,
+      and 1 number or special character)
+  }, on: :update, allow_blank: true
 
-	validates :first_name, presence: true, on: :create
-	validates :last_name, presence: true, on: :create
-	
+  validates :first_name, presence: true, on: :create
+  validates :last_name, presence: true, on: :create
+  
 end
