f7b86c51a0c8e746567f24a21a07e47d8c5fa9ba
André Franco <andre.gfranco@gmail.com>
2015-10-15 11:17:19 +0100

    Minor changes to application controller regarding coding style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u| 
-      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :email, 
+        :password, :password_confirmation)
     end
 
     devise_parameter_sanitizer.for(:account_update) do |u| 
-      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password) 
+      u.permit(:first_name, :last_name, :email, :password, 
+        :password_confirmation, :current_password) 
     end
   end
 end
