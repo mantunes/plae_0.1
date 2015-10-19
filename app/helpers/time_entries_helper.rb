9341127127bcd175d8bf7ca1ddc2bb78c45441d6
André Franco <andre.gfranco@gmail.com>
2015-10-19 17:28:17 +0100

    Fixed time_entry helper

--- app/helpers/time_entries_helper.rb
+++ app/helpers/time_entries_helper.rb
@@ -3,6 +3,12 @@ module TimeEntriesHelper
     hours = total_seconds / (60 * 60)
     minutes = (total_seconds / 60) % 60
     seconds = total_seconds % 60
-    "#{ hours } h #{ minutes } m #{ seconds } s"
+    if hours == 0 && minutes == 0
+      return "#{ seconds } s"
+    elsif hours == 0 
+      return "#{ minutes } m #{ seconds } s"
+    else
+      return "#{ hours } h #{ minutes } m #{ seconds } s"
+    end
   end
 end
