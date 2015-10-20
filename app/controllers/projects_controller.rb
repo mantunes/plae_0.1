62bd0f02e2bd586cfa403adc9d7cade328398ff4
André Franco <andre.gfranco@gmail.com>
2015-10-20 13:19:09 +0100

    Created views, routes and actions for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,18 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = current_user.projects
+    @memberships = current_user.memberships
+  end
+  def show
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
