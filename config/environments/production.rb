a878c08cc01acb9b21ee59f00c29b6a1308514b2
André Franco <andre.gfranco@gmail.com>
2015-10-26 15:41:43 +0000

    Configured action mailer in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'www.example.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key
+    :authentication => 'login',
+    :domain => 'heroku.com', # your domain to identify your server when connecting
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
