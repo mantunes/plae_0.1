0e43858801bb2f94166c821c01ba69b5ab696eeb
André Franco <andre.gfranco@gmail.com>
2015-11-09 15:40:41 +0000

    Fixed active admin css conflicts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.assets.precompile += ['active_admin.css']
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://rocky-wave-8276.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
