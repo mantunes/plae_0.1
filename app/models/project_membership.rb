5b5173fa50bc3385d736f14ed095dec6473701d3
André Franco <andre.gfranco@gmail.com>
2015-11-23 18:16:13 +0000

    added styles to index page

--- app/models/project_membership.rb
+++ app/models/project_membership.rb
@@ -3,6 +3,6 @@ class ProjectMembership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   validates :role, presence: true
-  validates_uniqueness_of :user_id, { scope: :project_id,
-                                      message: "User already in project" }
+  validates_uniqueness_of :user_id, scope: :project_id,
+                                    message: 'User already in project'
 end
