4962f99aa72449eac38b8d968256f30f139440e3
André Franco <andre.gfranco@gmail.com>
2015-12-03 12:15:26 +0000

    fixed report views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
   def index
     @time_entries = current_user.time_entries.where('end_time >= ?', 1.week.ago)
+    @time_entries = @time_entries.order('updated_at desc')
     @total_hours = formatted_duration(@time_entries.sum(:total_time).to_i)
   end
 
@@ -8,7 +9,9 @@ class ReportsController < ApplicationController
   end
 
   def create
-    @time_entries = get_entries(params[:projects], params[:start_time], params[:end_time])
+    @start_time = Time.zone.local(*params[:start_time].sort.map(&:last).map(&:to_i))
+    @end_time = Time.zone.local(*params[:end_time].sort.map(&:last).map(&:to_i))
+    @time_entries = get_entries(params[:projects], @start_time, @end_time)
     @time_entries = @time_entries.order('project_id')
     @total_hours = formatted_duration(@time_entries.sum(:total_time).to_i)
   end
@@ -23,8 +26,6 @@ class ReportsController < ApplicationController
   end
 
   def get_entries(projects, start_time, end_time)
-    start_time = Time.zone.local(*start_time.sort.map(&:last).map(&:to_i))
-    end_time = Time.zone.local(*end_time.sort.map(&:last).map(&:to_i))
     if projects.nil?
       return current_user.time_entries.where('end_time >= ? AND end_time <= ?',
                                              start_time, end_time)
