149bb97eb63ef8048e9a7eed55c333c2454a53d2
André Franco <andre.gfranco@gmail.com>
2015-12-07 17:40:03 +0000

    changed reports form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,38 +1,44 @@
 class ReportsController < ApplicationController
   def index
     @time_entries = current_user.time_entries.where('end_time >= ?', 1.week.ago)
-    @time_entries = @time_entries.order('updated_at desc')
-    @total_hours = formatted_duration(@time_entries.sum(:total_time).to_i)
-  end
-
-  def new
   end
 
   def create
     @start_time = Time.zone.local(*params[:start_time].sort.map(&:last).map(&:to_i))
     @end_time = Time.zone.local(*params[:end_time].sort.map(&:last).map(&:to_i))
-    @time_entries = get_entries(params[:projects], @start_time, @end_time)
-    @time_entries = @time_entries.order('project_id')
-    @total_hours = formatted_duration(@time_entries.sum(:total_time).to_i)
+    @time_entries = get_time_entries(params[:projects], params[:users], params[:without_project])
+    @time_entries = @time_entries.where('end_time >= ? AND end_time <= ?',
+                                        @start_time, @end_time)
   end
 
   private
 
-  def formatted_duration(total_seconds)
-    hours = total_seconds / (60 * 60)
-    minutes = (total_seconds / 60) % 60
-    seconds = total_seconds % 60
-    "#{hours} h #{minutes} m #{seconds} s"
+  def get_time_entries(projects, users, without_project)
+    if without_project == '1'
+      get_user_entries(users)
+    else
+      get_project_entries(projects, users)
+    end
   end
 
-  def get_entries(projects, start_time, end_time)
-    if projects.nil?
-      return current_user.time_entries.where('end_time >= ? AND end_time <= ?',
-                                             start_time, end_time)
+  def get_user_entries(users)
+    if users.present?
+      TimeEntry.where(user_id: users, project_id: nil)
     else
-      time_entries = TimeEntry.where(project_id: projects)
-      return time_entries.where('end_time >= ? AND end_time <= ?',
-                                  start_time, end_time)
+      current_user.time_entries.where(project_id: nil)
+    end
+  end
+
+  def get_project_entries(projects, users)
+    if projects.present? && users.nil?
+      TimeEntry.where(project_id: projects)
+    elsif projects.nil? && users.present?
+      projects = current_user.projects.map(&:id)
+      TimeEntry.where(user_id: users, project_id: projects)
+    elsif projects.present? && users.present?
+      TimeEntry.where(project_id: projects, user_id: users)
+    else
+      current_user.time_entries
     end
   end
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,9 @@
 module ReportsHelper
+
+  def formatted_duration(total_seconds)
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+    seconds = total_seconds % 60
+    hours.to_s + ':' + format('%02d', minutes.to_s) + ':' + format('%02d', seconds.to_s)
+  end
 end
